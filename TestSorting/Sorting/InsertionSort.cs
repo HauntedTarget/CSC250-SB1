4e77124f188f9fd6e2136b5b576ea4ffd3737b00
HauntedTarget <75811611+HauntedTarget@users.noreply.github.com>
2024-04-01 13:31:33 -0600

    Backing to the Selection & Insetion sorts

---
+++ TestSorting/Sorting/InsertionSort.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestSorting.Sorting
+{
+    [TestClass]
+    public class InsertionSort
+    {
+        
+        [TestMethod]
+        public void SortMethod()
+        {
+            //TODO: Make sorting algorithm
+        }
+    }
+}
