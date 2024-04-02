8eafb8587cdb9839ece297ad1c9063fc2e6cd561
HauntedTarget <75811611+HauntedTarget@users.noreply.github.com>
2024-04-02 14:19:16 -0600

    SelectionSort done TODO: Make proper unit testing forms.

--- TestSorting/Sorting/SelectionSort.cs
+++ TestSorting/Sorting/SelectionSort.cs
@@ -10,11 +10,61 @@ namespace TestSorting.Sorting
     public class SelectionSort
     {
         UnitData sortingTest = new();
+        int iterations = 0, lowestNum = 0;
 
         [TestMethod]
         public void SortMethod()
         {
-            //TODO: Make sorting algorithm
+            //First element of array is start.
+            /*
+             * int iterations = 0, lowestNum;
+             * 
+             * do{
+             *  
+             *  selectedItemNumber = item[iteration];
+             *  lowestNum = item[iteration];
+             *
+             *  int i = 0;
+             *  for ( i = iteration + 1; i < item.length; i++) [iteration = place to swap, i = place to check, lowestNum = lowest number found]
+             *  {
+             *      if ( item[i] < lowestNum )
+             *      {
+             *          lowestNum = item[i];
+             *      }
+             *  }
+             *  item[i] = item[iteration];
+             *  item[iteration] = lowestNum;
+             * 
+             * iterations++;
+             * 
+             * }while (while iterations < item.length)
+             */
+
+
+            do
+            {
+                int selectedItem = sortingTest.data[iterations];
+                int lowestNum = sortingTest.data[iterations];
+                int lowestI = 0 + iterations;
+
+                for (int i = 0 + iterations; i < sortingTest.data.Length; i++)
+                {
+                    if (sortingTest.data[i] < lowestNum)
+                    {
+                        lowestI = i;
+                        lowestNum = sortingTest.data[i];
+                    }
+                }
+
+                sortingTest.data[lowestI] = selectedItem;
+                sortingTest.data[iterations] = lowestNum;
+
+                iterations++;
+
+            } while (iterations < sortingTest.data.Length);
+
+            Console.WriteLine(sortingTest.data.ToString());
+
             Assert.AreEqual(sortingTest.CheckSort(), true);
         }
     }
